2b133eaafc041634aa6d472d0618048f8c70a5da
darja <darja.portnova@gmail.com>
2025-05-03 19:10:28 +0300

    fixed formatMeaning.ts

--- src/utils/format/formatMeanings.ts
+++ src/utils/format/formatMeanings.ts
@@ -3,10 +3,10 @@ import { Meaning, FormattedWord } from "@/types/wordType";
 export const formatMeanings = (meanings: Meaning[]): FormattedWord[] =>
   meanings.map((m): FormattedWord => ({
     id: m.id,
-    word: m.word.word,
-    audio: m.word.audio ?? null,
+    word: m.word,
+    audio: m.word ?? null,
     definition: m.definition,
-    partOfSpeech: m.partOfSpeech.title,
+    partOfSpeech: m.partOfSpeech,
     synonyms: m.synonyms,
     antonyms: m.antonyms,
     example: m.example,
